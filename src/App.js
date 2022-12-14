5238e0813cf3ad3c707252bc97d35d8199492e29
Flu3nt <gnedyalkov94@gmail.com>
2022-12-14 13:09:27 +0200

    Bugfix import Browser Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import PostsPage from "./pages/PostsPage";
 
 const App = () => {
   return (
-    <Router>
-      <Route exact path="/" component={DashboardPage} />
-      <Route exact path="/posts" component={PostsPage} />
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="posts" element={<PostsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
